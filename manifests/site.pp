0ce52f3d0fa67801130e603e64bcfcc9d2d9194b
FM_LAB <34754597+onlyNY@users.noreply.github.com>
2017-12-26 09:46:15 -0500

    Added Mac

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,3 +5,12 @@ node 'puppetagent-cent.lab' {
     #owner   => 'oy',
    }
 }
+
+node 'puppetagent-mac.local' {
+  file {'/Users/Shared/toolkit/icrm_local':
+    ensure  => file
+    content => 'find /Users/Shared/inCopy_Local -name "*.icml" -delete',
+    mode    => '0775',
+    owner   => 'macadmin',
+   }
+}
