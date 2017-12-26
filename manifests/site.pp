f4a1818132c64dc28815eb7eccdf20d7bf7ccf14
FM_LAB <34754597+onlyNY@users.noreply.github.com>
2017-12-26 12:05:06 -0500

    using modules
    removing the direct command from the site.pp and switching to modules... testing

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,7 @@
 node 'puppetagent-cent.lab' {
-  file {'/home/oy/blue.sh':
-    ensure  => file
-    #content => 'blue black red',
-    #owner   => 'oy',
-   }
+  include file_chk
 }
 
 node 'puppetagent-mac.local' {
-  file {'/Users/Shared/toolkit/icrm_local.sh':
-    ensure  => file
-   #content => 'find /Users/Shared/inCopy_Local -name "*.icml" -delete'
-   #mode    => '0775',
-   #owner   => 'macadmin',
-   }
+  include file_chk
 }
