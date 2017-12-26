8f0306aebf1b024a492f854a58f4e8688243eba0
FM_LAB <34754597+onlyNY@users.noreply.github.com>
2017-12-26 14:21:51 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,15 @@
 node 'puppetagent-cent.lab' {
-  notify { 'hello CENT':
+  notify { 'hello CENT': }
+  file { '/home/oy/fqdn.txt':
+  ensure  => file,
+  content => $fqdn,
   }
 }
 
 node 'puppetagent-mac.local' {
-  notify { 'hello MAC':
+  notify { 'hello MAC': }
+  file { '/Users/macadmin/Desktop/fqdn.txt':
+  ensure  => file,
+  content => $fqdn,
   }
 }
