9323fb813cf4d70cb1b70178a16c3fa29b0068a3
FM_LAB <34754597+onlyNY@users.noreply.github.com>
2017-12-26 12:03:36 -0500

    adding some mac specific checks

---
+++ modules/file_chk/manifests/file.pp
@@ -0,0 +1,8 @@
+node 'puppetagent-mac.local' {
+  file {'/Users/Shared/toolkit/icrm_local.sh':
+    ensure  => file
+   #content => 'find /Users/Shared/inCopy_Local -name "*.icml" -delete'
+   #mode    => '0775',
+   #owner   => 'macadmin',
+   }
+}
