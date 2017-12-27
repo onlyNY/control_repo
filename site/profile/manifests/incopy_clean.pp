c0e9e02c3bb5e9727b87f2bd5cb5849125ee96fa
FM_LAB <34754597+onlyNY@users.noreply.github.com>
2017-12-26 22:44:00 -0500

    command add

--- site/profile/manifests/incopy_clean.pp
+++ site/profile/manifests/incopy_clean.pp
@@ -1,3 +1,8 @@
 class profile::incopy_clean {
-  include incopy_clean
+  notify { 'hello MAC': }
+ #include role::incopy_clean
+  file { '/Users/macadmin/Desktop/fqdn.txt':
+  ensure  => file,
+  content => "Working on ${fqdn}\n",
+  }
 }
