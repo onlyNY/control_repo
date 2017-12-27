675761996c236ee2023a9fc4523ee5cdccee2a69
FM_LAB <34754597+onlyNY@users.noreply.github.com>
2017-12-27 08:57:27 -0500

    Update extensis_installer.pp

--- site/profile/manifests/extensis_installer.pp
+++ site/profile/manifests/extensis_installer.pp
@@ -1,4 +1,21 @@
-class profile::extensis_installer {
+class profile::extensis_installer (
+      $
+){
 
+  if $::osfamily == 'darwin' {
+       file { '/Library/Preferences/com.extensis.TypeServerCoreClient.conf': {
+       owner  => "$user",
+       mode   => 644,
+       ensure => present,
+       source => "puppet:///modules/extensis
+  }
+       file { '':
+       provider => dpkg,
+       ensure   => present,
+       source   => puppet:///modules/extensis/
 
+}elsif $::osfamily == 'windows' {
+
+}
+            
 }
