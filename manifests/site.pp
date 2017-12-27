79789c37803bafb900c79d72195e5ecad5ef85a9
FM_LAB <34754597+onlyNY@users.noreply.github.com>
2017-12-26 23:11:37 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,10 +7,6 @@ node 'puppetagent-cent.lab' {
 }
 
 node 'puppetagent-mac.local' {
-  notify { 'hello MAC': }
- #include role::incopy_clean
-  file { '/Users/macadmin/Desktop/fqdn.txt':
-  ensure  => file,
-  content => "Working on ${fqdn}\n",
+  include incoply_clean,
   }
 }
