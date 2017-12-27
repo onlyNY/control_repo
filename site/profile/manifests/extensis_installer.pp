c8e02968b8dceff0838f7799f3b2ecb5533fbff9
FM_LAB <34754597+onlyNY@users.noreply.github.com>
2017-12-27 11:07:11 -0500

    Update extensis_installer.pp

--- site/profile/manifests/extensis_installer.pp
+++ site/profile/manifests/extensis_installer.pp
@@ -1,21 +1,40 @@
 class profile::extensis_installer (
-      $
+      $installdir = 'puppet:///modules/extensis_installer/files/'
+      $Mpref      = '/Library/Preferences/com.extensis.TypeServerCoreClient.conf'
+      $Wpref      = 'C:\Program Files\Extensis\Type Server Core Client\
 ){
 
   if $::osfamily == 'darwin' {
-       file { '/Library/Preferences/com.extensis.TypeServerCoreClient.conf': {
+       file { $Mpref: 
        owner  => "$user",
        mode   => 644,
        ensure => present,
-       source => "puppet:///modules/extensis
+       source => "${installdir}/com.extensis.TypeServerCoreClient.conf",
+    }
   }
-       file { '':
+} 
+       package { 'Type Server Core Client':
        provider => dpkg,
-       ensure   => present,
-       source   => puppet:///modules/extensis/
+       ensure   => installed,
+       source   => "${installdir}/type.core_613M.pkg",
 
-}elsif $::osfamily == 'windows' {
-
-}
+} elsif $::osfamily == 'windows' {
+       file { $Wpref: 
+       owner  => "$user",
+       mode   => 644,
+       ensure => present,
+       source => "${installdir}/com.extensis.TypeServerCoreClient.conf",      
+      }
+ }
+ 
+      package { 'Core Client':
+       provider => dpkg,
+       ensure   => installed,
+       source   => "${installdir}/type.core_613W.exe",
             
-}
+} else {
+       notify 'This is not a supported distro.'
+ 
+ }
+ 
+ }
