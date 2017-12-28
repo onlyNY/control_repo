850139cb012b16232e5563f7747abcdd95b7b6bd
FM_LAB <34754597+onlyNY@users.noreply.github.com>
2017-12-28 15:23:12 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,12 +9,5 @@ node 'puppetagent-cent.lab' {
 node 'puppetagent-mac.local' {
   include norton_installer
 }
-
- # this is grabs any machine carrying imac in the hostname
- #  >> node /^imac/ {} <<
- # include  basic roll call
- # once a week
- # make trigger points - UPTIME | CPU | DED ACTIVITY
- 
  
  
