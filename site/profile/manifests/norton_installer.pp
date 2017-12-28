a703912444b454961f706b42c82ceafb42ca3a73
FM_LAB <34754597+onlyNY@users.noreply.github.com>
2017-12-28 14:39:51 -0500

    Update norton_installer.pp

--- site/profile/manifests/norton_installer.pp
+++ site/profile/manifests/norton_installer.pp
@@ -1,1 +1,17 @@
-class profile::norton_installer{ }
+class profile::norton_installer {
+        file { '/Users/Shared/toolkit/install_nort.sec':
+        ensure => directory,
+        mode => '0755',
+        owner => 'macuser',
+        group => 'staff',
+    }
+
+    file { "/Users/Shared/tooklkit/install_nort.sec/Install Norton Security.app":
+        mode => "0644",
+        owner => 'macuser',
+        group => 'staff',
+        source => 'puppet:///modules/nortons_installer/files/Install Norton Security.app',
+    }
+}
+
+}
